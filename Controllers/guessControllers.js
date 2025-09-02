a1928dcdf8530ded6d2515b5eac95800826a9065
unknown <elianjmarquezb@gmail.com>
2025-09-02 18:58:32 -0400

    Se ha agregado el controlador para obtener los vehiculos en general, por id y obtener todos los dealers

--- Controllers/guessControllers.js
+++ Controllers/guessControllers.js
@@ -1,9 +1,28 @@
+import { getGuessIndexService, getGuessCarService } from "../Services/guessServices.js";
+
 export const getGuessIndexController = async(req, res) => {
 try {
     const data = await getGuessIndexService()
-    res.status(200).json(data)
+    return res.status(200).json(data)
     
 } catch (error) {
-    return res.status(500).json({message: error.message})
+    return res.status(500).json(error)
 }
+}
+
+export const getGuessCarController = async(req, res) => {
+    try {
+        const { id } = req.params
+        const data = await getGuessCarService(id)
+        const result = data[0]
+        
+        if (!result) {
+            return res.status(404).json({ message: 'Vehiculo no encontrado' })
+        }
+        
+        return res.json(data)
+        
+    } catch (error) {
+        return res.status(500).json(error)
+    }
 }
