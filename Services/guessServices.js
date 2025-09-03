bfaac72c473b669638b9d7a99a090ecd3ec64f35
unknown <elianjmarquezb@gmail.com>
2025-09-02 21:52:35 -0400

    Se ha completado exitosamente la funcionalidad de registrar usuario

--- Services/guessServices.js
+++ Services/guessServices.js
@@ -77,21 +77,24 @@ export const getDealerService = (id) => {
 }
 
 export const registerService = (nombre, apellido, fecha_nacimiento, cedula, correo, usuario, hashedPassword) => {
-    return new Promise((resolve, reject) =>{
-        try {
-            const query = 'INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, cedula, correo) VALUES (?, ?, ?, ?, ?)'
-            const query2 = `INSERT INTO cuentas (usuario, clave, rol) VALUES (?, ?, 'vendedor')`
-            connection.query(query, [nombre, apellido, fecha_nacimiento, cedula, correo], (err, res) => {
-                if (err) {
-                   return reject(err)}
-            })
-            connection.query(query2, [usuario, hashedPassword], (err, res) => {
-                if (err) {
-                }
-               return resolve(res)
-            })
-        } catch (error) {
-            
-        }
-    })
-}
+  return new Promise((resolve, reject) => {
+    try {
+      const queryCuentas = `INSERT INTO cuentas (usuario, clave, rol) VALUES (?, ?, 'vendedor')`;
+      const queryUsuarios = 'INSERT INTO usuarios (nombre, apellido, fecha_nacimiento, cedula, correo, id_cuenta) VALUES (?, ?, ?, ?, ?, ?)';
+
+      connection.query(queryCuentas, [usuario, hashedPassword], (err, resCuentas) => {
+        if (err) return reject(err);
+
+        const idCuenta = resCuentas.insertId;
+
+        connection.query(queryUsuarios, [nombre, apellido, fecha_nacimiento, cedula, correo, idCuenta], (err, resUsuarios) => {
+          if (err) return reject(err);
+
+          return resolve({ cuentaId: idCuenta, usuarioId: resUsuarios.insertId });
+        });
+      });
+    } catch (error) {
+      return reject(error);
+    }
+  });
+};
