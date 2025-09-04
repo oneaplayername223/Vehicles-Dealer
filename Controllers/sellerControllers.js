8f7da0ee1bb7bf80592fe61ea819ecf86f2e8b98
unknown <elianjmarquezb@gmail.com>
2025-09-04 12:01:10 -0400

    Se ha mejorado la legibilidad y escalabilidad de el codigo, modularizando responsabilidades

--- Controllers/sellerControllers.js
+++ Controllers/sellerControllers.js
@@ -1,22 +1,21 @@
-import jwt from 'jsonwebtoken'
 import { postAddCarService, postviewCarService, postviewCarByIdService, postDeleteCarService, postEditCarService } from "../Services/sellerServices.js";
 import registerVehicleSchema from '../Schemas/registerVehicleSchema.js';
-import idSchema from '../Schemas/idSchema.js';
+
+
 export const postAddCarController = async(req, res) => {
+    const id_cuenta = req.id_cuenta
     try {
-        const token = req.cookies.token
-        const decode = jwt.verify(token, 'clave-secreta')
-        const id_cuenta = decode.dataCookie.id
+
         
         const {marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos} = req.body
 
         const { error } = registerVehicleSchema({marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos})
         if (error) {
-            return res.status(400).json(error)
+            return res.status(400).json({Mensaje: 'Ha habido un error'})
         }
 
         const data = await postAddCarService(marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos, id_cuenta)
-        console.log(`${[decode.dataCookie.id, decode.dataCookie.usuario]} ha añadido el vehiculo ${marca} ${modelo} exitosamente`)
+        console.log(`${id_cuenta} ha añadido el vehiculo ${marca} ${modelo} exitosamente`)
         return res.status(200).json({Mensaje: 'Vehiculo añadido correctamente'})
 
 
@@ -28,26 +27,22 @@ export const postAddCarController = async(req, res) => {
 
 
 export const postviewCarController = async(req, res) => {
+    const id_cuenta = req.id_cuenta
     try {
-        const token = req.cookies.token
-        const decode = jwt.verify(token, 'clave-secreta')
-        const id_cuenta = decode.dataCookie.id
+
 
         const data = await postviewCarService(id_cuenta)
         return res.status(200).json(data)
         
     } catch (error) {
-        return res.status(500).json(error)
+return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'})
         
     }
 }
 
 export const postviewCarByIdController = async(req, res) => {
-        const {id} = req.params  
-        const { error } = idSchema({id})
-    if (error) {
-        return res.status(400).json(error)
-    }
+        const {id} = req.params
+ 
     try {
     
         const data = await postviewCarByIdService(id)
@@ -59,74 +54,70 @@ export const postviewCarByIdController = async(req, res) => {
         return res.status(200).json(data)
         
     } catch (error) {
-        return res.status(500).json(error)
+                console.log(error)
+
+        return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'})
         
     }
 }
 export const postDeleteCarController = async(req, res) =>{
-    const token = req.cookies.token
     const {id} = req.params
-    const { error } = idSchema({id})
-    if (error) {
-        return res.status(400).json(error)
-    }
+    const id_cuenta = req.id_cuenta
     try {
-        const decode = jwt.verify(token, 'clave-secreta')
-        const id_cuenta = decode.dataCookie.id
+
         const data = await postDeleteCarService(id, id_cuenta)
         const affectedRows = data.affectedRows
 
         if (affectedRows === 0 || affectedRows > 1) {
-            return res.status(404).json({Mensaje: 'Vehiculo no encontrado'})
+            return res.status(204).json({Mensaje: 'Vehiculo no encontrado'})
         }     
 
 
-        console.log(`${[decode.dataCookie.id, decode.dataCookie.usuario]} ha eliminado el vehiculo ${id} exitosamente`)
+        console.log(`${id_cuenta} ha eliminado el vehiculo ${id} exitosamente`)
         return res.status(200).json({Mensaje: 'Vehiculo eliminado correctamente'})
         
     } catch (error) {
         console.log(error)
-        return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'})
+return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'})
     }
 }
 
 export const postEditCarController = async (req, res) => {
     //Client request
     const {marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos} = req.body
-    const {id} = req.params
     //Joi validations
     const { error } = registerVehicleSchema({marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos})
-    const { error2 } = idSchema({id})
-    //cookies request
-    const token = req.cookies.token
-    const decode = jwt.verify(token, 'clave-secreta')
-    const id_cuenta = decode.dataCookie.id
+    //cookies request    
+    const {id} = req.params
+    const id_cuenta = req.id_cuenta
+ 
 
-    if (error || error2) {
-        return res.status(400).json(error || error2)
+
+    if (error) {
+        console.log(error)
+        return res.status(400).json({Mensaje: 'Ha habido un error'})
     }
 
     try {
-        const { error } = idSchema({id, id_cuenta})
-    if (error) {
-        return res.status(400).json(error)
-    }
+   
     const data = await postEditCarService(marca, modelo, creado, color, categoria, traccion, pasajeros, descripcion, precio, imagenes, videos, id, id_cuenta)
    
-
     if(data.affectedRows === 1 && data.changedRows === 0){
-        return res.status(404).json({Mensaje: 'Ingresa la informacion nueva'})
+        return res.status(400).json({Mensaje: 'Ingresa la informacion nueva'})
     }
     if (data.changedRows === 1){
-    console.log(`${[decode.dataCookie.id, decode.dataCookie.usuario]} ha editado el vehiculo ${id} exitosamente`)
+    console.log(`${id_cuenta} ha editado el vehiculo ${id} exitosamente`)
     return res.status(200).json({Mensaje: 'Vehiculo editado correctamente'})
+    }
+    if (data.changedRows === 0 && data.affectedRows === 0){
+        return res.status(404).json({Mensaje: 'Vehiculo no encontrado'})
     }
 
 
    
     } catch (error) {
         console.log(error)
-        return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'}) 
+        return res.status(500).json({Mensaje: 'Ha habido un error en el servidor'})
         
     }
 }
