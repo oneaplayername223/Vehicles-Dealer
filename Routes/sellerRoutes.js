aa58a60033fe00fa6159208c56ebed80cd78c2c7
unknown <elianjmarquezb@gmail.com>
2025-09-04 11:59:48 -0400

    Se han agregado los middlewares de autorizacion y validacion a las rutas

--- Routes/sellerRoutes.js
+++ Routes/sellerRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { postAddCarController, postviewCarController, postviewCarByIdController, postDeleteCarController, postEditCarController } from "../Controllers/sellerControllers.js";
-
-
+import { userAuthorization } from "../middlewares/userAuth.js";
+import { validateId } from "../middlewares/validateId.js";
 const sellerRoutes = Router();
 
 
-sellerRoutes.post('/vendedor/agregar/autos', postAddCarController)
-sellerRoutes.get('/vendedor/autos', postviewCarController)
-sellerRoutes.get('/vendedor/autos/:id', postviewCarByIdController)
-sellerRoutes.delete('/vendedor/eliminar/autos/:id', postDeleteCarController)
-sellerRoutes.patch('/vendedor/editar/autos/:id', postEditCarController)
+sellerRoutes.post('/vendedor/agregar/autos', userAuthorization, validateId, postAddCarController)
+sellerRoutes.get('/vendedor/autos', userAuthorization, validateId,  postviewCarController)
+sellerRoutes.get('/vendedor/autos/:id', userAuthorization, validateId, postviewCarByIdController)
+sellerRoutes.delete('/vendedor/eliminar/autos/:id', userAuthorization, validateId, postDeleteCarController)
+sellerRoutes.patch('/vendedor/editar/autos/:id', userAuthorization, validateId, postEditCarController)
 
 export default sellerRoutes
